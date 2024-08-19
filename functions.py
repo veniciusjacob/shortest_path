84a1beb67581bcf073eaa4f9dc9c7a0456a3c528
veniciusjacob <79477926+veniciusjacob@users.noreply.github.com>
2024-08-19 13:28:51 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -3,41 +3,41 @@ import osmnx as ox
 import networkx as nx
 import matplotlib.pyplot as plt
 
-#incialização da API.
-def inicializar_cliente_google_maps():
-    gmaps = googlemaps.Client(key='API_KEY',)
+# Initialize the Google Maps API client.
+def initialize_google_maps_client():
+    gmaps = googlemaps.Client(key='API_KEY')
     return gmaps
 
-# essa função recebe um endereço e um cliente do Google Maps,
-# realiza a geocodificação desse endereço usando o cliente fornecido e retorna
-# as coordenadas geográficas correspondentes.
-def converter_endereco(gmaps, endereco):
-    geocode_result = gmaps.geocode(endereco)
-    coordenadas = geocode_result[0]['geometry']['location']
-    return coordenadas
+# This function receives an address and a Google Maps client,
+# geocodes the address using the provided client, and returns
+# the corresponding geographic coordinates.
+def geocode_address(gmaps, address):
+    geocode_result = gmaps.geocode(address)
+    coordinates = geocode_result[0]['geometry']['location']
+    return coordinates
 
-#esta função constrói um grafo de ruas para a cidade fornecida, utilizando a biblioteca OSMnx.
-def construir_grafo_de_ruas(cidade):
-    G = ox.graph_from_place(cidade, network_type='drive')
+# This function builds a street graph for the provided city using the OSMnx library.
+def build_street_graph(city):
+    G = ox.graph_from_place(city, network_type='drive')
     return G
 
-#essa função encontra o nós mais próximo das coordenadas informadas
-def encontrar_nos_mais_proximos(grafo, latitude, longitude):
-    no_mais_proximo = ox.distance.nearest_nodes(grafo, longitude, latitude)
-    return no_mais_proximo
+# This function finds the nearest node to the provided coordinates in the graph.
+def find_nearest_node(graph, latitude, longitude):
+    nearest_node = ox.distance.nearest_nodes(graph, longitude, latitude)
+    return nearest_node
 
-#esta função encontra e retorna o caminho mais curto entre
-#os nós de origem e destino no grafo de ruas, utilizando o algoritmo A*.
-def encontrar_caminho_mais_curto(grafo, origem, destino):
-    caminho_mais_curto = nx.astar_path(grafo, source=origem, target=destino, weight='length')
-    return caminho_mais_curto
+# This function finds and returns the shortest path between
+# the origin and destination nodes in the street graph using the A* algorithm.
+def find_shortest_path(graph, origin, destination):
+    shortest_path = nx.astar_path(graph, source=origin, target=destination, weight='length')
+    return shortest_path
 
-def plotar_grafo_com_rota(G, rota, coord_origem, coord_destino):
-    fig, ax = ox.plot_graph_route(G, rota, route_color='b', show=False, close=False)
+# This function plots the graph with the route, marking the origin and destination points.
+def plot_graph_with_route(G, route, origin_coords, destination_coords):
+    fig, ax = ox.plot_graph_route(G, route, route_color='b', show=False, close=False)
 
-    # Desenhar os pontos de origem e destino
-    plt.plot(coord_origem['lng'], coord_origem['lat'], 'ro')  # ponto A (origem) em vermelho
-    plt.plot(coord_destino['lng'], coord_destino['lat'], 'yo')  # ponto B (destino) em amarelo
+    # Draw the origin and destination points
+    plt.plot(origin_coords['lng'], origin_coords['lat'], 'ro')  # point A (origin) in red
+    plt.plot(destination_coords['lng'], destination_coords['lat'], 'yo')  # point B (destination) in yellow
 
     plt.show()
-
